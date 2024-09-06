f06435623bc1111f32256f8d7e97438eb3f45da5
YanGarrido <121405668+YanGarrido@users.noreply.github.com>
2024-09-06 18:07:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+function pesquisar() {
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  if(!campoPesquisa){
+    section.innerHTML = "Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte"
+    return
+}
+campoPesquisa = campoPesquisa.toLowerCase()
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let título = "";
+  let descricao ="";
+  let tags="";
+
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Cria um novo elemento HTML para cada resultado
+      resultados += `
+      <div class="item-resultado">
+        <img src="${dado.foto}" alt="${dado.titulo}">
+        <div class="item-texto">
+            <h2>${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Instagram</a>
+        </div>
+    </div>
+`;
+    }
+
+  }
+  if(!resultados){
+    resultados ="Nada foi encontrado!"
+  }
+
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,162 @@
+let dados = [
+  {
+    foto:"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/hamilton",
+    titulo:"Lewis Hamilton",  
+    descricao:"Sete vezes campeão mundial, Lewis Hamilton é um dos maiores pilotos da Fórmula 1 Conhecido por sua habilidade em ultrapassagens e consistência, ele continua sendo um dos favoritos Sua carreira é marcada por uma combinação de velocidade e estratégia incomparáveis.",
+    link: "https://www.instagram.com/lewishamilton/",
+    tags:"Fórmula 1, piloto, Lewis Hamilton, britânico, Mercedes-AMG Petronas, temporada 2024, campeão"
+      
+  },
+  {
+    foto:"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/verstappen",
+    titulo:"Max Verstappen",  
+    descricao:"Campeão mundial em 2021 e 2022, Max Verstappen é conhecido por sua agressividade e habilidade em condições difíceis. Sua capacidade de tirar o máximo de cada corrida o coloca frequentemente no topo das classificações.",
+    link: "https://www.instagram.com/maxverstappen1/",
+    tags:"Fórmula 1, piloto, Max Verstappen, holandês, Red Bull Racing, temporada 2024, campeão"
+      
+  },
+  {
+    foto:"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/leclerc",
+    titulo:"Charles Leclerc",  
+    descricao:"Charles Leclerc é um jovem talento com grande potencial. Com várias vitórias e pole positions, ele é conhecido por sua velocidade pura e habilidade em tirar o melhor de seu carro.",
+    link: "https://www.instagram.com/charles_leclerc/",
+    tags:"Fórmula 1, piloto, Charles Leclerc, monegasco, Ferrari, temporada 2024"
+      
+  },
+  {
+    foto:"https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/russell",
+    titulo:"George Russell",  
+    descricao:"George Russell é um dos pilotos mais promissores da nova geração. Sua habilidade em se adaptar rapidamente às condições da pista e seu estilo agressivo fazem dele um forte concorrente.",
+    link: "https://www.instagram.com/georgerussell63/",
+    tags:"Fórmula 1, piloto, George Russell, britânico, Mercedes-AMG Petronas, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/perez",
+    titulo: "Sergio Pérez", 
+    descricao: "Sergio Pérez, conhecido por seu talento em gestão de pneus e consistência, tem sido um piloto crucial para a Red Bull Racing. Ele é altamente valorizado por sua capacidade de maximizar o desempenho da equipe.",
+    link: "https://www.instagram.com/schecoperez/",
+    tags: "Fórmula 1, piloto, Sergio Pérez, mexicano, Red Bull Racing, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/sainz",
+    titulo: "Carlos Sainz", 
+    descricao: "Carlos Sainz é conhecido por sua habilidade técnica e consistência. Ele frequentemente desafia os líderes da corrida e é um ativo valioso para a Ferrari.",
+    link: "https://www.instagram.com/carlossainz55/",
+    tags: " Fórmula 1, piloto, Carlos Sainz, espanhol, Ferrari, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/norris",
+    titulo: "Lando Norris", 
+    descricao: "Lando Norris é um dos pilotos mais emocionantes e promissores da Fórmula 1. Seu estilo de pilotagem agressivo e seu talento para a comunicação com a equipe são amplamente reconhecidos.",
+    link: "https://www.instagram.com/landonorris/",
+    tags: "Fórmula 1, piloto, Lando Norris, britânico, McLaren, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/piastri",
+    titulo: "Oscar Piastri", 
+    descricao: "Estreante em 2023, Oscar Piastri mostrou uma impressionante habilidade e adaptação. Ele é um dos novos talentos a serem observados na Fórmula 1.",
+    link: "https://www.instagram.com/oscarpiastri/",
+    tags: "Fórmula 1, piloto, Oscar Piastri, australiano, McLaren, temporada 2024, estreante"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/alonso",
+    titulo: "Fernando Alonso", 
+    descricao: "Bicampeão mundial, Fernando Alonso continua a impressionar com sua habilidade técnica e experiência. Sua capacidade de desafiar os limites e sua consistência são altamente respeitadas.",
+    link: "https://www.instagram.com/fernandoalo_oficial/",
+    tags: "Fórmula 1, piloto, Fernando Alonso, espanhol, Aston Martin, temporada 2024, campeão"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/drivers/2024Drivers/stroll",
+    titulo: "Lance Stroll", 
+    descricao: "Lance Stroll, filho do proprietário da equipe, é conhecido por suas fortes performances em qualificações e sua habilidade em gerir corridas complexas.",
+    link: "https://www.instagram.com/lance_stroll/",
+    tags: "Fórmula 1, piloto, Lance Stroll, canadense, Aston Martin, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/tsunoda",
+    titulo: "Yuki Tsunoda", 
+    descricao: "Yuki Tsunoda é conhecido por sua agressividade e talento jovem. Ele tem mostrado um bom desempenho em sua equipe, e seu estilo de pilotagem é altamente técnico.",
+    link: "https://www.instagram.com/yukitsunoda0511/",
+    tags: "Fórmula 1, piloto, Yuki Tsunoda, japonês, Visacashapp, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/ricciardo",
+    titulo: "Daniel Ricciardo", 
+    descricao: "Daniel Ricciardo, conhecido por suas habilidades em ultrapassagens e seu carisma, retorna à Fórmula 1 para mais uma temporada. Ele é altamente respeitado por sua velocidade e experiência.",
+    link: "https://www.instagram.com/danielricciardo/",
+    tags: "Fórmula 1, piloto, Daniel Ricciardo, australiano, Visacashapp, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/bottas",
+    titulo: "Valtteri Bottas", 
+    descricao: "Valtteri Bottas, ex-piloto da Mercedes, é conhecido por sua consistência e velocidade. Ele traz uma riqueza de experiência e é um forte concorrente em todas as corridas.",
+    link: "https://www.instagram.com/valtteribottas/",
+    tags: "Fórmula 1, piloto, Valtteri Bottas, finlandês, Kick Sauber, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/zhou",
+    titulo: "Zhou Guanyu", 
+    descricao: "Zhou Guanyu é o primeiro piloto chinês a competir na Fórmula 1. Ele mostrou um progresso constante e é conhecido por sua habilidade em se adaptar rapidamente às mudanças.",
+    link: "https://www.instagram.com/zhouguanyu24/",
+    tags: "Fórmula 1, piloto, Zhou Guanyu, chinês, Kick Sauber, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/ocon",
+    titulo: "Esteban Ocon ", 
+    descricao: "Esteban Ocon é conhecido por sua tenacidade e habilidades de pilotagem agressivas. Ele frequentemente desempenha um papel crucial na luta por posições.",
+    link: "https://www.instagram.com/estebanocon/",
+    tags: " Fórmula 1, piloto, Esteban Ocon, francês, Alpine, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/gasly",
+    titulo: "Pierre Gasly", 
+    descricao: "Pierre Gasly, ex-piloto da AlphaTauri, é conhecido por seu estilo de pilotagem ousado e sua habilidade em aproveitar as oportunidades.",
+    link: "https://www.instagram.com/pierregasly/",
+    tags: "Fórmula 1, piloto, Pierre Gasly, francês, Alpine, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/albon",
+    titulo: "Alexander Albon", 
+    descricao: "Alexander Albon retorna à Fórmula 1 com a Williams. Conhecido por sua habilidade em ultrapassagens e sua consistência, ele traz uma abordagem técnica para a equipe.",
+    link: "https://www.instagram.com/alex_albon/",
+    tags: "Fórmula 1, piloto, Alexander Albon, tailandês, Williams, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/drivers/2024Drivers/colapinto",
+    titulo: "Franco Colapinto", 
+    descricao: "Franco Colapinto é um jovem talento argentino que está começando sua jornada na Fórmula 1. Conhecido por sua velocidade e habilidades de adaptação, ele promete trazer uma nova energia para a equipe.",
+    link: "https://www.instagram.com/francolapinto/",
+    tags: "Fórmula 1, piloto, Franco Colapinto, argentino, Williams, temporada 2024, estreante"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/hulkenberg",
+    titulo: "Nico Hülkenberg", 
+    descricao: "Nico Hülkenberg, com vasta experiência e um forte histórico na Fórmula 1, retorna para mais uma temporada com a Haas. Ele é conhecido por sua habilidade técnica e consistência.",
+    link: "https://www.instagram.com/hulkhulkenberg/",
+    tags: "Fórmula 1, piloto, Nico Hülkenberg, alemão, Haas, temporada 2024"
+      
+  },
+  {
+    foto: "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/drivers/2024Drivers/magnussen",
+    titulo: "Kevin Magnussen", 
+    descricao: "Kevin Magnussen é conhecido por sua agressividade e habilidade em adaptar-se rapidamente às condições da pista. Ele traz uma experiência valiosa para a equipe Haas.",
+    link: "https://www.instagram.com/kevinmagnussen/",
+    tags: "Fórmula 1, piloto, Kevin Magnussen, dinamarquês, Haas, temporada 2024"
+      
+  },
+];
